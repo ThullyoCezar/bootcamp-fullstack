ff1c7a01989b37d31ddbfb0e87d0797746ffd493
Thullyo.pequeno <thullyocezar12@gmail.com>
2024-05-02 09:46:29 -0300

    commit

---
+++ aula/app.js
@@ -0,0 +1,17 @@
+// 1.Variáveis  
+// 2. Tipo (número , string , booleano, etc)
+// 3. Operadores lógicos 
+// 4. Estruturas condicinais 
+// 5. Operações matemáticas 
+// 6. funções
+// 7. Array 
+// 8. Objetos
+
+// 1. Crie uma lista de objetos com itens de mercado que contenha nome, preços e quantidade de cada um .
+// no fim , exiba a lista completa com o valor total de compra.
+// * A lista deve ter no minimo 2 ITENS.
+//** O valor total deve ser exibido com pelo menos 2 casas decimais.
+
+var carrinho = carrinho = [
+
+]
